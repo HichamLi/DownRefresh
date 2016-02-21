9786d8926be3f728dc60e88ade7b902291640d7a
HichamLi <li_hai_qiang@outlook.com>
2016-02-21 20:08:20 +0800

    增加自定义view

--- app/src/main/java/com/itql/downrefresh/activities/MainActivity.java
+++ app/src/main/java/com/itql/downrefresh/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.itql.downrefresh;
+package com.itql.downrefresh.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.itql.downrefresh.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/itql/downrefresh/customs/ListHeadView.java
@@ -0,0 +1,65 @@
+package com.itql.downrefresh.customs;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import com.itql.downrefresh.R;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class ListHeadView extends ListView {
+
+    public ListHeadView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ListHeadView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ListHeadView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init(){
+
+    }
+
+    class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+
+}
