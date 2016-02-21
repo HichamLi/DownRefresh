2a3a9ed35f928a2047cc68cb40b248f6edadba5f
HichamLi <li_hai_qiang@outlook.com>
2016-02-22 01:14:35 +0800

    准备设置动画

--- app/src/main/java/com/itql/downrefresh/activities/MainActivity.java
+++ app/src/main/java/com/itql/downrefresh/activities/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.RotateAnimation;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -14,9 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView lv;
     private View headerView;
+    private View footerView;
+
+    private ImageView iv_rotate;
+    private ImageView iv_refresh;
 
     private String tvString[];
     private MyAdapter myAdapter;
+
+    private RotateAnimation ra_iv_rotate;
+    private RotateAnimation ra_iv_refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +38,10 @@ public class MainActivity extends AppCompatActivity {
     private void init(){
         lv= (ListView) findViewById(R.id.lv);
         headerView=View.inflate(MainActivity.this,R.layout.view_list_head,null);
+        footerView=View.inflate(MainActivity.this,R.layout.view_list_foot,null);
 
-        lv.addHeaderView(headerView,null,true);
+        lv.addHeaderView(headerView);
+        lv.addFooterView(footerView);
 
         tvString=new String[30];
         for(int i=0;i<tvString.length;i++){
@@ -38,6 +49,17 @@ public class MainActivity extends AppCompatActivity {
         }
         myAdapter = new MyAdapter();
         lv.setAdapter(myAdapter);
+
+        iv_rotate= (ImageView) findViewById(R.id.iv_rotate);
+        iv_refresh= (ImageView) findViewById(R.id.iv_refresh);
+    }
+
+    private void initAnimation(){
+        ra_iv_rotate = new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        ra_iv_refresh = new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+
+        ra_iv_rotate.setDuration(1);
+
     }
 
     class MyAdapter extends BaseAdapter{
