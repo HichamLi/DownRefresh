abc5b4c9ff5d5b6c97f987e4f9d1b0270d290b0e
HichamLi <li_hai_qiang@outlook.com>
2016-02-27 17:35:30 +0800

    继续完善ListHeadView

--- app/src/main/java/com/itql/downrefresh/customs/ListHeadView.java
+++ app/src/main/java/com/itql/downrefresh/customs/ListHeadView.java
@@ -1,17 +1,14 @@
 package com.itql.downrefresh.customs;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.text.TextPaint;
+
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.itql.downrefresh.R;
 
@@ -21,6 +18,26 @@ import com.itql.downrefresh.R;
 public class ListHeadView extends ListView {
 
     private View headerView;
+    private ImageView iv_rotate;
+    private ProgressBar pb_refresh_head;
+    private TextView tv_down_refresh_head;
+    private TextView tv_refresh_time;
+    private int headerViewHeight;
+
+    private View footerView;
+    private ProgressBar pb_refresh_foot;
+    private TextView tv_down_refresh_foot;
+    private int footerViewHeight;
+
+    private int downY;                          //按下时Y坐标
+
+    private final int PULL_REFRESH = 0;         //下拉刷新的状态
+    private final int RELEASE_REFRESH = 1;      //松开刷新的状态
+    private final int REFRESHING = 2;           //正在刷新的状态
+    private int currentState = PULL_REFRESH;
+
+    private RotateAnimation upAnimation;        //向上的旋转动画
+    private RotateAnimation downAnimation;      //向下的旋转动画
 
     public ListHeadView(Context context) {
         super(context);
@@ -37,8 +54,13 @@ public class ListHeadView extends ListView {
         init();
     }
 
+    /**
+     * 初始化
+     */
     private void init(){
         initHeaderView();
+        initFooterView();
+        initAnimation();
     }
 
     /**
@@ -46,29 +68,46 @@ public class ListHeadView extends ListView {
      */
     private void initHeaderView() {
         headerView = View.inflate(getContext(), R.layout.view_list_head, null);
+        iv_rotate= (ImageView) headerView.findViewById(R.id.iv_rotate);
+        pb_refresh_head= (ProgressBar) headerView.findViewById(R.id.pb_refresh_head);
+        tv_down_refresh_head= (TextView) headerView.findViewById(R.id.tv_down_refresh_head);
+        tv_refresh_time= (TextView) headerView.findViewById(R.id.tv_refresh_time);
+
+        headerView.measure(0,0);                        //主动通知系统去测量该view;
+        headerViewHeight=headerView.getMeasuredHeight();
+        headerView.setPadding(0, -headerViewHeight, 0, 0); //设置padding为负数，可以隐藏headerView
+
+        addHeaderView(headerView);
     }
 
-    class MyAdapter extends BaseAdapter{
+    /**
+     * 初始化 footView
+     */
+    private void initFooterView(){
+        footerView = View.inflate(getContext(), R.layout.view_list_foot, null);
+        pb_refresh_foot= (ProgressBar) footerView.findViewById(R.id.pb_refresh_foot);
+        tv_down_refresh_foot= (TextView) footerView.findViewById(R.id.tv_down_refresh_foot);
 
-        @Override
-        public int getCount() {
-            return 0;
-        }
+        footerView.measure(0,0);                        //主动通知系统去测量该view;
+        footerViewHeight=footerView.getMeasuredHeight();
+        footerView.setPadding(0,-footerViewHeight,0,0); //设置padding为负数，可以隐藏headerView
 
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
-        }
+        addFooterView(footerView);
     }
+
+    /**
+     * 初始化旋转动画
+     */
+    private void initAnimation(){
+        upAnimation = new RotateAnimation(0, -180, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        upAnimation.setDuration(300);
+        upAnimation.setFillAfter(true);
+
+        downAnimation = new RotateAnimation(-180, -360, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        downAnimation.setDuration(300);
+        downAnimation.setFillAfter(true);
+    }
+
+
 
 }
